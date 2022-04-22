943d66e79c899b84d04f476eade616bc528cad35
vitamaxoduol <vitamaxoduo@gmail.com>
2022-04-23 00:03:08 +0300

    commit changes

--- write_funcs.c
+++ write_funcs.c
@@ -11,20 +11,20 @@
  */
 int _putchar(char c)
 {
-static char buf[1024];
-static int i;
+    static char buf[1024];
+    static int i;
 
-if (c == -1 || i >= 1024)
-{
-write(1, &buf, i);
-i = 0;
-}
-if (c != -1)
-{
-buf[i] = c;
-i++;
-}
-return (1);
+    if (c == -1 || i >= 1024)
+    {
+        write(1, &buf, i);
+        i = 0;
+    }
+    if (c != -1)
+    {
+        buf[i] = c;
+        i++;
+    }
+    return (1);
 }
 
 /**
@@ -34,10 +34,9 @@ return (1);
  */
 int _puts(char *str)
 {
-register int i;
+    register int i;
 
-for (i = 0; str[i] != '\0'; i++)
-putchar(str[i]);
-return (i);
+    for (i = 0; str[i] != '\0'; i++)
+        _putchar(str[i]);
+    return (i);
 }
-
